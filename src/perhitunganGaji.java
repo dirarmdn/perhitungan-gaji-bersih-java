c229a26e671e9236cdff9c51c39aa8e3e1e7d424
dirarmdn <dhiraramadini@gmail.com>
2022-11-19 16:17:51 +0700

    update mybe

--- src/perhitunganGaji.java
+++ src/perhitunganGaji.java
@@ -7,9 +7,9 @@ public class perhitunganGaji {
         int gol, gaji_kotor, tunj_mrt, tunj_ank, mar, ank;
         double pajak, gaji_bersih;
 
-        int golongan[] = {0, 2000000, 2500000, 3000000, 3500000};
-        int marital[] = {0, 300000, 500000, 800000, 1000000};
-        int anak[] = {0, 1, 2};
+        int[] golongan = {0, 2000000, 2500000, 3000000, 3500000};
+        int[] marital = {0, 300000, 500000, 800000, 1000000};
+        int[] anak = {0, 1, 2};
 
         BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
         Scanner sc = new Scanner(System.in);
@@ -30,36 +30,37 @@ public class perhitunganGaji {
             ank = sc.nextInt();
             System.out.println("----------------------------");
 
-            switch (gol){
-                case 1 :
+            switch (gol) {
+                case 1 -> {
                     gaji_kotor = golongan[gol];
                     tunj_mrt = marital[mar];
                     tunj_ank = 500000 * anak[ank];
                     pajak = gaji_kotor * 0.025;
-                    break;
-                case 2 :
+                }
+                case 2 -> {
                     gaji_kotor = golongan[gol];
                     tunj_mrt = marital[mar];
                     tunj_ank = 750000 * anak[ank];
                     pajak = gaji_kotor * 0.05;
-                    break;
-                case 3 :
+                }
+                case 3 -> {
                     gaji_kotor = golongan[gol];
                     tunj_mrt = marital[mar];
                     tunj_ank = 1000000 * anak[ank];
                     pajak = gaji_kotor * 0.07;
-                    break;
-                case 4 :
+                }
+                case 4 -> {
                     gaji_kotor = golongan[gol];
                     tunj_mrt = marital[mar];
                     tunj_ank = 1250000 * anak[ank];
                     pajak = gaji_kotor * 0.1;
-                    break;
-                default:
+                }
+                default -> {
                     gaji_kotor = 0;
                     tunj_ank = 0;
                     tunj_mrt = 0;
                     pajak = 0;
+                }
             }
 
             gaji_bersih = gaji_kotor + tunj_mrt + tunj_ank - pajak;
